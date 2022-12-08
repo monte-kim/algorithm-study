7a81dbf1cc920a97297e79f23b54f6ed9b45be0e
Monte <monte6198@gmail.com>
2022-12-09 01:50:06 +0900

    DAY 23

--- Programmers/Lv. 1/같은 숫자는 싫어.js
+++ Programmers/Lv. 1/같은 숫자는 싫어.js
@@ -1,13 +1,12 @@
 function solution(arr) {
   // 효율성 테스트에서는 여기가 더 빨랐다.
-  var answer = [];
+  const answer = [];
   arr.forEach((number) => {
     if (answer.length === 0) {
       answer.push(number);
-    } else {
-      if (answer[answer.length - 1] !== number) {
-        answer.push(number);
-      }
+    }
+    if (answer.length > 0 && answer[answer.length - 1] !== number) {
+      answer.push(number);
     }
   });
   return answer;
@@ -18,3 +17,6 @@ function solution(arr) {
 //         return number !== arr[index + 1]
 //     })
 // }
+
+// A: [1, 3, 0, 1]
+solution([1, 1, 3, 3, 0, 1, 1]);
