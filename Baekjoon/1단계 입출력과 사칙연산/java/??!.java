a388332718f3952f2fe55f1ccdc7e57c98752120
taehwan01 <monte6198@gmail.com>
2024-01-10 23:57:22 +0900

    java 1단계 3~9

---
+++ Baekjoon/1단계 입출력과 사칙연산/java/??!.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String id = br.readLine();
+        System.out.println(id + "??!");
+    }
+}
