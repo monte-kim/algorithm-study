618688a772b4cf3915498a60b48baa471d1f423f
taehwan01 <monte6198@gmail.com>
2024-02-09 23:41:51 +0900

    java 10단계 4,5,6

---
+++ Baekjoon/java/10단계/삼각형 외우기.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+
+        if (a + b + c != 180) {
+            System.out.println("Error");
+        } else if (a == 60 && b == 60 && c == 60) {
+            System.out.println("Equilateral");
+        } else if (a != b && b != c && c != a) {
+            System.out.println("Scalene");
+        } else {
+            System.out.println("Isosceles");
+        }
+    }
+}
