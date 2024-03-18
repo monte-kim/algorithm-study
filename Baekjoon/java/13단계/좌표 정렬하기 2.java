fa14fa1a1262b67448f9ab01bf1085b99e15a739
taehwan01 <monte6198@gmail.com>
2024-03-18 13:05:38 +0900

    java 13단계 8

---
+++ Baekjoon/java/13단계/좌표 정렬하기 2.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        List<Point> points = new ArrayList<>();
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            points.add(new Point(x, y));
+        }
+
+        Collections.sort(points);
+
+        for (Point point : points) {
+            sb.append(point.x).append(" ").append(point.y).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    static class Point implements Comparable<Point> {
+        private int x;
+        private int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Point other) {
+            return this.y == other.y ? this.x - other.x : this.y - other.y;
+        }
+    }
+}
