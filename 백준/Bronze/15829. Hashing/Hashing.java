26ae61e210f1188b9edc290a50da0bdb6f4351d1
taehwan <111658806+taehwan01@users.noreply.github.com>
2024-08-16 16:01:11 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14324 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -3,27 +3,28 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.math.BigInteger;
 
 public class Main {
-	private static char[] alphabets = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-		'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int r = 31;
-		int M = 1234567891;
-		int result = 0;
+		long M = 1234567891;
+		long result = 0;
+		long powValue = 1;
 
 		int L = Integer.parseInt(br.readLine());
-		char[] letters = br.readLine().toCharArray();
+		String word = br.readLine();
 
 		for (int i = 0; i < L; i++) {
-			int index = (int)letters[i] - 96;
-			result += index * (int)Math.pow(r, i);
+			result += (word.charAt(i) - 96) * powValue;
+			powValue = powValue * r % M;
 		}
 
-		bw.write(String.valueOf(result % M));
+		result %= M;
+
+		bw.write(String.valueOf(result));
 		bw.flush();
 		bw.close();
 		br.close();
