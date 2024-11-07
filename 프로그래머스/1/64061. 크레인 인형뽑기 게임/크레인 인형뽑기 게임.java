47c247eb60e03b6d97dd2e2cc497400c3bba8668
taehwan <111658806+taehwan01@users.noreply.github.com>
2024-11-08 02:35:20 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.43 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -2,28 +2,37 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] board, int[] moves) {
-        Stack<Integer> pickedDolls = new Stack<>();
-        int count = 0;
+        Stack<Integer>[] dolls = new Stack[board.length];
 
-        for (int move : moves) {
-            for (int i = 0; i < board.length; i++) {
-                if (board[i][move - 1] != 0) {
-                    int pickedDoll = board[i][move - 1];
+        for (int i = 0; i < board.length; i++) {
+            dolls[i] = new Stack<>();
+        }
 
-                    if (!pickedDolls.isEmpty() && pickedDolls.peek() == pickedDoll) {
-                        pickedDolls.pop();
-                        count++;
-                        count++;
-                    } else {
-                        pickedDolls.push(pickedDoll);
-                    }
-
-                    board[i][move - 1] = 0;
-                    break;
+        for (int i = board.length - 1; i >= 0; i--) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] != 0) {
+                    dolls[j].push(board[i][j]);
                 }
             }
         }
 
+        Stack<Integer> pickedDolls = new Stack<>();
+        int count = 0;
+
+        for (int move : moves) {
+            if (!dolls[move - 1].isEmpty()) {
+                int pickedDoll = dolls[move - 1].pop();
+
+                if (!pickedDolls.isEmpty() && pickedDolls.peek() == pickedDoll) {
+                    pickedDolls.pop();
+                    count++;
+                    count++;
+                } else {
+                    pickedDolls.push(pickedDoll);
+                }
+            }
+        }
+        
         return count;
     }
 }
