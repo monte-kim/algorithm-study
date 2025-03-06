7ac1fc380531423fee7ddad355ab5db03bc8fc07
taehwan01 <monte6198@gmail.com>
2025-03-07 02:48:06 +0900

    코딩 테스트 합격자 되기 [그래프 - 문제 36] 다익스트라 알고리즘

--- Others/src/코딩테스트합격자되기/Dijkstra.java
+++ Others/src/코딩테스트합격자되기/Dijkstra.java
@@ -1,6 +1,8 @@
 package 코딩테스트합격자되기;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 // [그래프 - 문제 36] 다익스트라 알고리즘
 public class Dijkstra {
@@ -12,7 +14,55 @@ public class Dijkstra {
         System.out.println(Arrays.toString(solution(graph2, 0, 4)));
     }
 
+    private static class Node implements Comparable<Node> {
+
+        int index;
+        int cost;
+
+        public Node(int destination, int cost) {
+            this.index = destination;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.cost - o.cost;
+        }
+    }
+
     private static int[] solution(int[][] graph, int start, int n) {
-        return new int[0];
+        ArrayList<Node>[] map = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            map[i] = new ArrayList<>();
+        }
+        for (int[] edge : graph) {
+            map[edge[0]].add(new Node(edge[1], edge[2]));
+        }
+
+        boolean[] visited = new boolean[n];
+        int[] distance = new int[n];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        distance[start] = 0;
+
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(start, 0));
+
+        while (!pq.isEmpty()) {
+            Node currentNode = pq.poll();
+
+            if (visited[currentNode.index]) {
+                continue;
+            }
+            visited[currentNode.index] = true;
+
+            for (Node nextNode : map[currentNode.index]) {
+                if (distance[nextNode.index] > nextNode.cost + currentNode.cost) {
+                    distance[nextNode.index] = nextNode.cost + currentNode.cost;
+                    pq.add(new Node(nextNode.index, distance[nextNode.index]));
+                }
+            }
+        }
+
+        return distance;
     }
 }
